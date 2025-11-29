790144acc6fe69b0ce174b64ea6aadcbcc6c2e94
Gauri Ganjoo <gauriganjoo@berkeley.edu>
2025-11-28 22:43:42 -0800

    Clean up edf_rename.py by removing comments
    Removed commented-out code for year input and file operations.

--- edf_rename.py
+++ edf_rename.py
@@ -21,32 +21,6 @@ for index, row in df.iterrows():
 		print(box_path+df.iloc[index,0], 'not found')
 
 
-# import os
-# import pandas as pd
-
-# year = input("Please enter the year (e.g., 2016): ")
-# box_path = '/Users/gganjoo/Library/CloudStorage/Box-Box/mignot-sleep-DATA/BIDMC_transfer/Domino_EDFs(2015-present)/'+year+'/'
-# # box_path = '/Users/gganjoo/Downloads/2023_staging'
-
-# # Load the CSV file containing the renaming information
-# df = pd.read_csv("renamingnow.csv")
-
-# for index, row in df.iterrows():
-#     old_name = os.path.join(box_path, df.iloc[index, 0])
-#     new_name = os.path.join(box_path, df.iloc[index, 1])
-    
-#     try:
-#         # Rename the directory
-#         os.rename(old_name, new_name)
-#         print(f"{old_name} renamed to {new_name}")
-#     except FileNotFoundError:
-#         print(f"{old_name} not found")
-#     except OSError as e:
-#         print(f"Error renaming {old_name} to {new_name}: {e}")
-
-
-
-
 
 
 
