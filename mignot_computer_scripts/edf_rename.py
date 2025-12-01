aecdd8f9632840389f23f7ab36b4e7bdb8149963
Gauri Ganjoo <gauriganjoo@berkeley.edu>
2025-12-01 10:52:24 -0800

    Clean up whitespace and formatting in edf_rename.py

--- mignot_computer_scripts/edf_rename.py
+++ mignot_computer_scripts/edf_rename.py
@@ -3,7 +3,6 @@
 @date: 2025-Nov-28
 @description: This script performs renames files on box.
 """
-
 import os
 import pandas as pd
 
@@ -12,15 +11,10 @@ box_path = '/Users/{sunnet_id}/Library/CloudStorage/Box-Box/rest of path to file
 
 df = pd.read_csv("renamingnow.csv")
 
-
 for index, row in df.iterrows():
 	try:
 		os.rename(box_path+df.iloc[index,0], box_path+df.iloc[index,1])
 		print(str(df.iloc[index,0]), 'renamed to', str(df.iloc[index,1]))
 	except FileNotFoundError:
 		print(box_path+df.iloc[index,0], 'not found')
-
-
-
-
 
